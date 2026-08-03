5f785fff29ed348db3a3398de4f2595bfcdab11c
BiBoy <bibowen@126.com>
2026-08-03 11:52:13 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       {/* 视频背景 */}
       <video
         className="absolute inset-0 z-0 h-full w-full object-cover"
-        src="https://s3-public-presigner-production-bbf5.up.railway.app/Hero-Section.mp4"
+        src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260314_131748_f2ca2a28-fed7-44c8-b9a9-bd9acdd5ec31.mp4"
         autoPlay
         loop
         muted
